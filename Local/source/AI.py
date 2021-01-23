af17ea957d3fe8a2cf3da3d54b60be91cfb48a06
jakel <jakelucasnyc@gmail.com>
2021-01-22 21:57:32 -0500

    Directory Name Change

---
+++ Local/source/AI.py
@@ -0,0 +1,33 @@
+import random
+
+class AI:
+
+    START_CORD = 40
+    END_CORD = 760
+    BOARD_SIZE = END_CORD-START_CORD
+
+    def __init__(self, side):
+        self.side = side
+        self.clicked = False
+        self.mouse_pos = None
+        self.AI_turn = False
+        self.game_moves = 0
+        self.game_over = False
+
+    def get_game_moves(self, game_moves):
+        self.game_moves = game_moves
+        if self.game_moves % 2 == self.side-1 and self.game_over == False:
+            self.AI_turn = True
+        else:
+            self.AI_turn = False
+
+    def get_info(self, game_over):
+        self.game_over = game_over
+
+    def get_mouse_pos(self):
+        while (self.game_moves % 2 == self.side-1):
+            move_cords = [random.randint(AI.START_CORD, AI.END_CORD), random.randint(AI.START_CORD, AI.END_CORD)]
+
+            return move_cords
+
+    
---
+++ Local/source/Button.py
@@ -0,0 +1,33 @@
+import pygame
+
+class Button:
+
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+
+    def __init__(self, x, y, width, height, color, font, text, screen):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.font = font
+        self.text = text
+        self.screen = screen
+
+    def draw_button(self):
+        pygame.draw.rect(self.screen, self.color, (self.x, self.y, self.width, self.height))
+        rendered_text = self.font.render(self.text, True, Button.WHITE)
+        # if self.width > rendered_text.get_rect().width:
+        #     text_cords = [int(self.x + ((self.width-rendered_text.get_rect().width)/2)), int(self.y + ((self.height-rendered_text.get_rect().height)/1.5))] #there's a 1.5 here because the buttons look better that way
+        # else:
+        #     raise Exception('The button rect must be wider than the text width')
+        text_cords = [int(self.x + ((self.width-rendered_text.get_rect().width)/2)), int(self.y + ((self.height-rendered_text.get_rect().height)/1.5))]
+        self.screen.blit(rendered_text, text_cords)
+
+    def is_clicked(self, mouse_pos):
+        if mouse_pos[0] > self.x and mouse_pos[0] < self.x + self.width and mouse_pos[1] > self.y and mouse_pos[1] < self.y + self.height:
+            return True
